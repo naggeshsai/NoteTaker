947f86b4677510f632d84653a8b76a28c14b3ef5
Naggesh Sai <naggesh.sai@gmail.com>
2016-09-01 15:59:51 +0530

    First Commit of Android NoteTaker

--- app/src/main/java/com/example/naggeshk/notetaker/EditNoteActivity.java
+++ app/src/main/java/com/example/naggeshk/notetaker/EditNoteActivity.java
@@ -73,6 +73,7 @@ public class EditNoteActivity extends AppCompatActivity {
                 finish();
             }
         });
+        //Some Changes
         bSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
